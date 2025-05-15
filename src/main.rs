bb75e0588d406ee7c3c41aa210385bb70a34af08
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-15 18:00:21 +0530

    structs but have to learn tuple and unit struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-
+struct User{
+    email:String,
+    password:u32
+}
 fn main(){
-    let mut s1=String::from("anubhav");
-    let s2=&s1;//borrowing s1
-    println!("{}",s1);
-    borrowing(&mut s1);
+    
+let user1=User{
+    email:String::from("anubhav"),
+    password:5,
+};
+println!("{}",user1.email)
+// tuple struct, unit struct have to learn it
 }
-fn borrowing(s: &mut String){
-s.push_str("purohit");
-}
-
-//you can do 1 mut borrow and u cannot also dont have immutable reference either
-//  but if you want to only borrow value u can many
