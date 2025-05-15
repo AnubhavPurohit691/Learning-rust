b94ce9daaf395c3b0a7eeb38abe0b40fe49ed425
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-15 17:45:15 +0530

    again borrow

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,5 @@ fn borrowing(s: &mut String){
 s.push_str("purohit");
 }
 
-//you can do 1 mut borrow but if you want to only borrow value u can many
+//you can do 1 mut borrow and u cannot also dont have immutable reference either
+//  but if you want to only borrow value u can many
