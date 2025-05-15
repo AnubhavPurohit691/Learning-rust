c37f5ea0ad06c394d73c0e2bbf37e83c77b43346
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-15 11:25:19 +0530

    memory safety string

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-    let ans = sum(5,3);
-    println!("{}",ans); //this is how you get variable in rust .
-    println!("{}",is_even(ans));
-    stringtest();
-    vec();
-
-    // for loop
-
-    for i in 0..100{
-        println!("{}",i);
+fn main(){
+    let greeting = "anubhav";
+    let char1= greeting.chars().nth(0);
+    match char1 {
+        Some(c)=>println!("{}",c),
+        None=>println!("no character found at")
     }
+    
 }
-// integer
-fn sum(a:i32,b:i32)->i32{
-return a + b;
-}
-// boolean
-
-fn is_even(a:i32)->bool{
-return a%2 ==0;
-}
-
-// strings
-
-fn stringtest(){
-    let name=String::from("anubhav");
-    println!("fristname - {}",name);
-}
-// vector
-
-fn vec(){
-    let v = vec![1,2,3];
-    println!("{:?}",v);
-    }
