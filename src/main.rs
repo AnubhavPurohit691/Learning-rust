513cc833064c4962f6b6a92df5f41e876c93c18a
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-15 22:26:28 +0530

    option enum

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::fs;
-
-fn main(){
-let res=fs::read_to_string("example.txt");
-match res {
-    Ok(content)=>Ok(content),
-    Err(_)=>Err("error reading file".to_string())
-};
-println!("printing")
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, character) in s.chars().enumerate() {
+        if character == 'a' {
+            return Some(index as i32);
+        }
+    }
+    return None;
 }
-// go to pdf projects.100xdevs for more example
+
+fn main() {
+    let my_string = String::from("raman");
+    match find_first_a(my_string) {
+        Some(index) => println!("The letter 'a' is found at index: {}", index),
+        None => println!("The letter 'a' is not found in the string."),
+    }
+}
