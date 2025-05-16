5c3eb6067b061bfad203e2ec9f831da7881e5952
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-16 14:45:23 +0530

    vector in rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-use rand::Rng;
-
-fn main() {
-    let mut rng = rand::rng(); // ✅ use `rng()` instead of deprecated `thread_rng()`
-    let n: u32 = rng.random();    // ✅ specify the type explicitly
-    println!("random number: {}", n);
+fn main (){
+    let mut vec= Vec::new();
+    let number = vec![1,2,4];//another way
+    println!("{:?}",number);
+    vec.push(1);
+    vec.push(2);
+    println!("{:?}",vec);//vec are structs.
+    let nvec=new_vec(vec);
+    println!("{:?}",nvec);
+    // println!("{:?}",vec);//vec are structs.
+}
+fn new_vec(vec:Vec<i32>)->Vec<i32>{
+    let mut nvec=Vec::new();
+    for val in vec{
+        if val%2==0{
+            nvec.push(val);
+        }
+    }
+return nvec
 }
