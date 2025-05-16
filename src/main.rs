fb32bb42b8eb014d003d436acabd748cdeda1fec
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-16 07:53:27 +0530

    random number program

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-fn find_first_a(s: String) -> Option<i32> {
-    for (index, character) in s.chars().enumerate() {
-        if character == 'a' {
-            return Some(index as i32);
-        }
-    }
-    return None;
-}
+use rand::Rng;
 
 fn main() {
-    let my_string = String::from("raman");
-    match find_first_a(my_string) {
-        Some(index) => println!("The letter 'a' is found at index: {}", index),
-        None => println!("The letter 'a' is not found in the string."),
-    }
+    let mut rng = rand::rng(); // ✅ use `rng()` instead of deprecated `thread_rng()`
+    let n: u32 = rng.random();    // ✅ specify the type explicitly
+    println!("random number: {}", n);
 }
