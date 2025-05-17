261d36ef812de3d8b9faf629e96270a3b78c5a2c
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-17 16:32:22 +0530

    message sending problem

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use core::error;
 use std::{sync::mpsc, thread::spawn};
 
-fn main (){
+fn main(){
     let (tx,rx)=mpsc::channel();
-    spawn(move ||{
-        tx.send(String::from("hello"));
-    });
-    let value=rx.recv();
-    match value{
-        Ok(value)=>println!("{}",value),
-        Err(err)=>println!("error while reading")
+    for i in 0..10{
+        let producer=tx.clone();
+        spawn(move||{
+            let mut sum:i64=0;
+            for j in i*10000000..(i+1*10000000)-1{
+                sum=sum+j;
+            }
+            producer.send(sum).unwrap()
+        });
     }
+    drop(tx); //after thread done its work it will stop this is required other wise consumer think it still sending tx sending the value
+    let mut finalsum:i64=0;
+    for val in rx{
+        finalsum=finalsum+val
+    }
+    println!("{}",finalsum)
+
 }
