e4f35dcc7f5065c9d96a5a58a7b56f73efdd8679
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-17 15:21:54 +0530

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-//lifetime in structs
+use std::thread;
 
-use std::fmt::Display;
-
-
-// struct User<'a>{
-//     name:&'a str
-// }
-// fn main(){
-// let firstname=String::from("anubhav");
-// let user=User{
-//     name:&firstname
-// };
-// println!("{}",user.name);
-// }
 fn main(){
-let xname=String::from("anubhav");
-let yname=String::from("purohit4");
-let ans =longest_with_an_announcement(&xname, &yname, "Comparing names");
-println!("{}",ans)
-}
-fn longest_with_an_announcement<'a,T>(x:&'a str,y:&'a str,ann:T)->&'a str where T:Display,{
-println!("Announcement! {ann}");
-if x.len()>y.len(){
-    return x
-}
-else{
-    return y
-}
-}
+let handle=thread::spawn(||{
+    for _i in 0..500000000{
+        println!("hi from spawed");
+    }
+});
 
+for _i in 0..500000000{
+    println!("hi from main")
+};
+handle.join().unwrap();
+}
