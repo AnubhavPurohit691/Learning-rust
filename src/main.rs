45e7b185055936d819413b41e5ebbf29fd76c379
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-17 13:12:17 +0530

    struct with lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+//lifetime in structs
+
+use std::fmt::Display;
+
+
+// struct User<'a>{
+//     name:&'a str
+// }
+// fn main(){
+// let firstname=String::from("anubhav");
+// let user=User{
+//     name:&firstname
+// };
+// println!("{}",user.name);
+// }
 fn main(){
-    let longest;
-    let smallstring=String::from("small");
-    {
-        let longerstring=String::from("longer");
-        longest=longestString(&smallstring,&longerstring);
-        println!("{}",longest);
-    }
+let xname=String::from("anubhav");
+let yname=String::from("purohit4");
+let ans =longest_with_an_announcement(&xname, &yname, "Comparing names");
+println!("{}",ans)
+}
+fn longest_with_an_announcement<'a,T>(x:&'a str,y:&'a str,ann:T)->&'a str where T:Display,{
+println!("Announcement! {ann}");
+if x.len()>y.len(){
+    return x
+}
+else{
+    return y
+}
 }
 
-fn longestString<'a>(small: &'a String, long: &'a String) -> &'a String { //lifetime 'a where ->'a is intersection of both lifespan.
-    if small.len() > long.len() {
-        return small;
-    } else {
-        return long;
-    }
-}
