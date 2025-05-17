c0029521ca26d2eb296599e905b297a92bb0d7d5
anubhavpurohit <anubhavpurohitno1@gmail.com>
2025-05-17 09:20:56 +0530

    slice

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,18 @@
-use std::{iter, vec};
-
-fn main(){
-    let  v1 = vec![12,34,34];
-   consuming_adapter( &v1);
-   iterator_adapter(&v1);  // this both change vec to iterator 
-   
-  let vecfromiter= iter_to_vec(&v1);
-  println!("vecfrom iter{:?}",vecfromiter)
+fn main (){
+    // slice
+    let  str=String::from("anubhav purohit");
+    let word = &str[0..8];
+    println!("{}",word);
+    let ans = first_word(&str);
+    println!("{}",ans);
 }
-
-
-fn consuming_adapter(v1:& Vec<i32>){
-     // consuming adapters
-     
-     let v1_iter=v1.iter();
-     let sumsed:i32 = v1_iter.sum();
-     println!("{}",sumsed);
- 
-     // but this take ownership so you can't use this v1_iter so u can't use it again further.
+fn first_word(str :&String)->&str{
+let mut spaceindex=0;
+for i in str.chars(){
+    if i == ' '{
+         break;
 }
-
-fn iterator_adapter(v1 :& Vec<i32>){
-// iterator adapter.
-let iter= v1.iter();
-// let iter2=iter.map(|x|x+1);
-let iter2=iter.filter(|x| *x %2 ==0); 
-// here |x | is input and *x is reference of reference value
-for v in iter2{
-    println!("{}",v);
-}
-// this map iterator return another iterator which we can use to iterate and it doesn't take ownership .
-}
-
-fn iter_to_vec(v1 :& Vec<i32>)->Vec<i32>{
-let itervec = v1.iter().map(|x| *x +1);
-let vec_iter:Vec<i32>= itervec.collect();
-return vec_iter
+spaceindex=spaceindex+1;
+};
+return &str[0..spaceindex];
 }
